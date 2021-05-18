1b670e9087cd55dcdeec425f63af933003aa9579
Taylor Waddell <itaylorw251@gmail.com>
2021-05-18 11:37:33 -0500

    forgot a few lines in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
+const knex = require("./db/connection");
 const express = require("express");
 const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
