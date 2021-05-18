e6bd35db68759d3bd2fb65cd013f326a5ee3692f
Taylor Waddell <itaylorw251@gmail.com>
2021-05-18 11:38:56 -0500

    created asyncErrorBoundary

---
+++ back-end/src/errors/asyncErrorBoundary.js
@@ -0,0 +1,10 @@
+export default function asyncErrorBoundary(delegate, defaultStatus) {
+  return (request, response, next) => {
+    Promise.resolve()
+      .then(() => delegate(request, response, next))
+      .catch((error = {}) => {
+        const { status = defaultStatus, message = error } = error;
+        next({ status, message });
+      });
+  };
+}
