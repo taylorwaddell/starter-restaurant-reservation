6e7ccdb620e6efc9dcddbd38d20b60481ad63eb9
Taylor Waddell <itaylorw251@gmail.com>
2021-05-19 09:52:12 -0500

    fixed migrations, seeds, and added fixtures.

---
+++ back-end/src/db/fixtures/01-tables.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    table_name: "#1",
+    capacity: 2,
+  },
+  {
+    table_name: "#2",
+    capacity: 2,
+  },
+  {
+    table_name: "#3",
+    capacity: 4,
+  },
+  {
+    table_name: "#4",
+    capacity: 6,
+  },
+];
